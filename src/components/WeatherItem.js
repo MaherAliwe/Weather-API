20371f91b12b00a1087b56ac3f5518fe69fc7335
MaherAliwe <83904987+MaherAliwe@users.noreply.github.com>
2021-06-09 23:54:04 +0300

    Update WeatherItem.js

--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-
+import './WeatherItem.css';
+import FakeWeather from '../fakeWeatherData.json';
+import storm from "../img/weather-icons/storm.svg"
 export default class WeatherItem extends Component {
-  render() {
-    return <div>Heello</div>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
   }
-}
-export class SayHello extends Component {
   render() {
     return (
-      <div style={{ color: this.props.color, backgroundColor: "yellow" }}>
-        Hello {this.props.name}
+      <div>
+       <main>
+         <div className="app__main">
+           <img style={{width:"300px"}} src={this.props.getIcon(this.props.id)}></img>
+           <h2>{this.props.tempertureMin}<span>&#176;</span> to {this.props.tempertureMax}<span>&#8451;</span></h2>
+           <p><span style={{fontWeight:"bold" , marginRight:"1rem"}}>Humidity</span> {this.props.humid}%  <span style = {{fontWeight:"bold" , margin:"0 1rem"}}>Pressure</span> 
+           {this.props.pressure}</p>
+         </div>
+        </main>
       </div>
     );
   }
